46340f867b546fba0acae19927e0abff21d3ce46
Zerossik <zeross19940303@gmail.com>
2023-11-12 20:14:23 +0200

    add routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,21 @@
 const contactsRouter = require('express').Router();
-const { isAuthenticated, validateBody } = require('../../middleware');
+const {
+  isAuthenticated,
+  validateBody,
+  isValidObjtId,
+} = require('../../middleware');
 const { ContactsController } = require('../../controllers');
 const { joiContactSchema } = require('../../models');
 
 contactsRouter.use(isAuthenticated);
 
 contactsRouter.get('/', ContactsController.getAll);
+contactsRouter.get('/:id', isValidObjtId, ContactsController.getContactById);
 contactsRouter.post(
   '/',
   validateBody(joiContactSchema),
   ContactsController.addContact
 );
+contactsRouter.delete('/:id', isValidObjtId, ContactsController.deleteContact);
+contactsRouter.patch('/:id', ContactsController.updateContact);
 module.exports = contactsRouter;
